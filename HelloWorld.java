ecb2cb6da8c197d78a2fabd52237b6d1f0cec7cf
Nithin Battu <103623085+Nithin003@users.noreply.github.com>
2026-09-09 20:00:43 +0530

    Fix typo in System.out.println statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Poll SCM builder - 01 ");
-        Syetem.out.println("GitSCM Pooling");
+        System.out.println("GitSCM Pooling");
     }
 }
