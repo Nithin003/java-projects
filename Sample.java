5dc098ad35d20b580f1e53e4396ec72df5850fc8
Nithin Battu <103623085+Nithin003@users.noreply.github.com>
2026-09-15 19:26:02 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,9 +1,5 @@
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("GitSCM Pooling");
-        System.out.println("GitSCM Pooling - 02");
-        System.out.println("GitSCM Pooling - 03");
-        System.out.println("GitSCM Pooling - 04");
         System.out.println("New job created 03");
         System.out.println("New job created 04");
     }
